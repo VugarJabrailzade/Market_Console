0d1788152f4dcbe6ec643599d0fd32948be7c818
Vugar Jabrailzade <vugarej@code.edu.az>
2023-07-21 18:19:05 +0400

    Creating Program and SubMenu class

---
+++ MarketConsole/MarketConsole/HelpMenu/SubMenu.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketConsole.HelpMenu
+{
+    public class SubMenu
+    {
+        public static void ProductHandling()
+        {
+            int option;
+
+            do
+            {
+
+            
+                Console.WriteLine("1. Add new product.");
+                Console.WriteLine("2. Update product.");
+                Console.WriteLine("3. Remove product.");
+                Console.WriteLine("4. Show category by product.");
+                Console.WriteLine("5. Show product by price range.");
+                Console.WriteLine("6. Find product by name.");
+                Console.WriteLine("0. Back to main menu.");
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Please, enter a valid option:");
+                Console.WriteLine("------------------------");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                   Console.WriteLine("------------------------");
+                   Console.WriteLine("Please, enter a valid option:");
+                   Console.WriteLine("------------------------");
+                }
+
+           
+                switch(option)
+                {
+                    case 1:
+                       break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("No such option!");
+                        break;
+                }
+
+            } while(option != 0);
+
+        }
+
+        public static void SalesHandling()
+        {
+
+            int option;
+
+            do
+            {
+
+                Console.WriteLine("1. Add new sales.");
+                Console.WriteLine("2. Return purchase.");
+                Console.WriteLine("3. Remove sales.");
+                Console.WriteLine("4. Show all sales.");
+                Console.WriteLine("5. Show sales by date.");
+                Console.WriteLine("6. Show sales by amount range.");
+                Console.WriteLine("7. Search by sale date.");
+                Console.WriteLine("8. Show sales by ID.");
+                Console.WriteLine("0. Back to main menu.");
+                Console.WriteLine("------------------------");
+                Console.WriteLine("Please, enter a valid option:");
+                Console.WriteLine("------------------------");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("------------------------");
+                    Console.WriteLine("Please, enter a valid option:");
+                    Console.WriteLine("------------------------");
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break; 
+                    case 8:
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine("No such option!");
+                        break;
+                }
+
+
+            }while(option != 0);
+        }
+
+
+
+
+    }
+}
---
+++ MarketConsole/MarketConsole/Program.cs
@@ -0,0 +1,48 @@
+﻿using MarketConsole.HelpMenu;
+
+namespace MarketConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int option;
+
+            do
+            {
+                Console.WriteLine("1. Product Handling:");
+                Console.WriteLine("2. Sales Handling");
+                Console.WriteLine("3. Exit.");
+                Console.WriteLine("----------------");
+                Console.WriteLine("Enter an option please:");
+                Console.WriteLine("----------------");
+
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Invalid option!");
+                    Console.WriteLine("Enter an option please:");
+                    Console.WriteLine("----------------");
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        SubMenu.ProductHandling();
+                        break;
+                    case 2:
+                        SubMenu.SalesHandling();
+                        break;
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("No such option!");
+                        break;
+                }
+
+            } while (option!=0);
+
+            
+        }
+    }
+}
